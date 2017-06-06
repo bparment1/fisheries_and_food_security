620c4b9a62e2a14aa3ed096fef10a325b49d6fdf
bparment1 <benoit.parmentier@gmail.com>
2017-06-06 14:30:00 -0400

    initial commit for reading in data from survey in Magadascar

---
+++ processing_data_magadascar_fisheries.R
@@ -0,0 +1,130 @@
+############### SESYNC Research Support: Fisheries and food security ########## 
+## Importing and processing data from survey for the fisheries project at SESYNC.
+## 
+## DATE CREATED: 06/06/2017
+## DATE MODIFIED: 06/06/2017
+## AUTHORS: Benoit Parmentier and Elizabeth Daut 
+## Version: 1
+## PROJECT: Fisheries by Jessica Gephart
+## ISSUE: 
+## TO DO:
+##
+## COMMIT: initial commit
+##
+## Links to investigate:
+
+###################################################
+#
+
+###### Library used
+
+library(gtools)                              # loading some useful tools 
+library(sp)                                  # Spatial pacakge with class definition by Bivand et al.
+library(spdep)                               # Spatial pacakge with methods and spatial stat. by Bivand et al.
+library(rgdal)                               # GDAL wrapper for R, spatial utilities
+library(gdata)                               # various tools with xls reading, cbindX
+library(rasterVis)                           # Raster plotting functions
+library(parallel)                            # Parallelization of processes with multiple cores
+library(maptools)                            # Tools and functions for sp and other spatial objects e.g. spCbind
+library(maps)                                # Tools and data for spatial/geographic objects
+library(plyr)                                # Various tools including rbind.fill
+library(spgwr)                               # GWR method
+library(rgeos)                               # Geometric, topologic library of functions
+library(gridExtra)                           # Combining lattice plots
+library(colorRamps)                          # Palette/color ramps for symbology
+library(ggplot2)
+
+###### Functions used in this script sourced from other files
+
+#function_rainfall_time_series_NEST_analyses <- "rainfall_time_series_NEST_function_12112015.R" #PARAM 1
+#script_path <- "/home/bparmentier/Google Drive/NEST/R_NEST" #path to script #PARAM 
+#script_path <- "/home/parmentier/Data/rainfall/NEST"
+#source(file.path(script_path,function_rainfall_time_series_NEST_analyses)) #source all functions used in this script 1.
+
+##### Functions used in this script 
+
+create_dir_fun <- function(outDir,out_suffix=NULL){
+  #if out_suffix is not null then append out_suffix string
+  if(!is.null(out_suffix)){
+    out_name <- paste("output_",out_suffix,sep="")
+    outDir <- file.path(outDir,out_name)
+  }
+  #create if does not exists
+  if(!file.exists(outDir)){
+    dir.create(outDir)
+  }
+  return(outDir)
+}
+
+#Used to load RData object saved within the functions produced.
+load_obj <- function(f){
+  env <- new.env()
+  nm <- load(f, env)[1]
+  env[[nm]]
+}
+
+## Add processing of downloaded files!!
+## 1) crop and reproject if needed
+## 2) creation of TimeRaster object with summaries?
+## 3)
+### Other functions ####
+
+#function_processing_data <- "processing_data_google_search_time_series_functions_06012017b.R" #PARAM 1
+script_path <- "/nfs/bparmentier-data/Data/projects/Fisheries_and_food_security/scripts" #path to script #PARAM 
+#source(file.path(script_path,function_processing_data)) #source all functions used in this script 1.
+
+############################################################################
+#####  Parameters and argument set up ###########
+
+in_dir <- "/nfs/bparmentier-data/Data/projects/Fisheries_and_food_security/workflow_preprocessing/data" #local bpy50 , param 1
+#in_dir <- "/home/parmentier/Data/rainfall/NEST" #NCEAS, param 
+out_dir <- "/nfs/bparmentier-data/Data/projects/Fisheries_and_food_security/workflow_preprocessing/outputs" #param 2
+
+date_val <- "2017-04-15"
+
+num_cores <- 2 #param 8
+create_out_dir_param=TRUE # param 9
+
+NA_value <- -9999 # param 10
+NA_flag_val <- NA_value #param 11
+
+out_suffix <-"processing_fisheries_magadascar_06062017" #output suffix for the files and ouptut folder #param 12
+
+#download_file <- FALSE #param 14
+#unzip_files <- F #param 15
+
+############## START SCRIPT ############################
+
+######### PART 0: Set up the output dir ################
+
+if(is.null(out_dir)){
+  out_dir <- in_dir #output will be created in the input dir
+  
+}
+#out_dir <- in_dir #output will be created in the input dir
+
+out_suffix_s <- out_suffix #can modify name of output suffix
+if(create_out_dir_param==TRUE){
+  out_dir <- create_dir_fun(out_dir,out_suffix)
+  setwd(out_dir)
+}else{
+  setwd(out_dir) #use previoulsy defined directory
+}
+
+### PART I READ AND PREPARE DATA #######
+#set up the working directory
+#Create output directory
+
+lf <- list.files(in_dir)
+
+##first unzip
+
+
+### then read in
+
+### Add quote="" otherwise EOF warning and error in reading
+df <- read.table(file.path(in_dir,infile_name),sep=",",fill=T,quote="",header=F)
+
+
+
+#############
